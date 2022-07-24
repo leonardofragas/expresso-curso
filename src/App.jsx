c7bef0bc954d52165def727acdf029c3f08b31cf
fragas <leonardosouzafragas@gmail.com>
2022-07-24 14:32:49 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,168 @@
+import { useState } from 'react'
+import * as React from 'react';
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Pricing from './Pricing';
+
+const courseModules = [
+  {
+    "img": "module-01.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-02.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-03.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-04.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-05.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-06.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-07.png",
+    "title": "o que faz um gestor de projetos?"
+  },
+
+  {
+    "img": "module-08.png",
+    "title": "o que faz um gestor de projetos?"
+  }
+]
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className="hero">
+        <div className="content">
+          <Container maxWidth="sm">
+            <h1>gestão eficiente & criativa para o mercado digital.</h1>
+            <h2>esteja preparado para a próxima revolução do marketing digital. aprenda a gerenciar campanhas, produtos e processos sendo um gestor de projeto criativo.</h2>
+            <a href="#">entrar agora</a>
+          </Container>
+        </div>
+      </div>
+
+      <div className="vsl">
+        <div className="content">
+          <Container>
+            <Row>
+              <h1>estamos diante da primeira evolução criativa do mercado digital.</h1>
+              <h2>seja um dos primeiros a ditar as regras do mercado com campanhas criativas.</h2>
+              <div className="responsive-video"><iframe src="https://youtube.com/embed/9oKmRY5QwYU" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+            </Row>
+          </Container>
+        </div>
+      </div>
+
+      <div className="oportunidade">
+
+      </div>
+
+      <div className="para-quem-e">
+      </div>
+
+      <div className="conteudo-do-curso">
+        <Container>
+          <div className="content">
+            <Row>
+              <h1>conheça tudo o que você vai receber no curso expresso de gestão</h1>
+            </Row>
+
+            <Row>
+              {courseModules.map((item) => (
+                <Col>
+                  <img
+                    src={`${item.img}`}
+                    srcSet={`${item.img}`}
+                    alt={item.title}
+                  />
+                </Col>
+              ))}
+            </Row>
+          </div>
+        </Container>
+      </div>
+
+      <div className="Pricing">
+        <Container>
+          <Row style={{ marginBottom: '80px' }}>
+            <h1>comece, hoje, a conquistar o destaque no mercado que você precisa.</h1>
+            <div className="Stroke"></div>
+          </Row>
+
+          <Row>
+            <Col xs={1} sm={4}></Col>
+            <Col>
+              <Pricing />
+            </Col>
+            <Col xs={1} sm={4}></Col>
+
+          </Row>
+        </Container>
+      </div>
+
+      <div className="bonus">
+
+      </div>
+
+      {/* <div className="rodape-1">
+        <Container>
+          <Row>
+            <Col>
+                
+            </Col>
+            <Col>
+              <ul>
+                <li><a href="#">Início</a></li>
+                <li><a href="mailto:fragas@escolaolhar.com">Suporte</a></li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </div> */}
+
+      <div className="rodape-2">
+        <Container>
+          <Row>
+            <Col xs={12} md={6}>
+              <p>
+                Fragas Inc. 2022 | Todos os direitos reservados.
+              </p>
+            </Col>
+            <Col xs={12} md={6}>
+              <p>
+                Itajaí – SC | 27.158.830/0001-06
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Pricing.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
+import Button from 'react-bootstrap/Button';
+
+import ListItem from './ListItem'
+
+import Data from './pricing.json'
+
+import './Pricing.styl'
+
+export default function Pricing() {
+
+  return (
+    <div className="Table">
+      <Container fluid>
+        <div className="Header">
+          <Row style={{ alignItems: 'center', width: '100%' }}>
+            <Col>
+              <h2>Turma 1</h2>
+              <h3>Pagamento em até 12x de 39,86 BRL.</h3>
+            </Col>
+
+            <Col style={{ textAlign: 'right' }}>
+              <h4>12 de R$ 150,60</h4>
+            </Col>
+          </Row>
+        </div>
+
+        <div className="Features">
+          <Row>
+            <ul>
+
+              {Data.mainContent.map((object, i) => {
+                return <ListItem category="conteúdo principal" text={object.title} key={object.title}/>
+              })}
+
+              {Data.oneYearOfSchool.map((object, i) => {
+                return <ListItem category="(de R$ 397) por R$ 0" text={object.title} key={object.title}/>
+              })}
+
+              {Data.bonuses.map((object, i) => {
+                return <ListItem category="bônus" text={object.title} key={object.title}/>
+              })}
+
+              {Data.masterclasses.map((object, i) => {
+                return <ListItem category="masterclass" text={object.title} key={object.title}/>
+              })}
+
+              {Data.extras.map((object, i) => {
+                return <ListItem category="extra" text={object.title} key={object.title}/>
+              })}
+            </ul>
+          </Row>
+        </div>
+
+        <div className="Call">
+          <Row>
+              <Button href={Data.checkout.year.url}>entrar agora</Button>
+          </Row>
+        </div>
+      </Container>
+    </div>
+  )
+}
